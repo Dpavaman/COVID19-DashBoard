fb353acac83a64471b3f30f41606768007cf0027
Dpavaman <67939138+Dpavaman@users.noreply.github.com>
2020-10-27 11:42:03 +0530

    Initial Commit

---
+++ src/Components/index.js
@@ -0,0 +1,5 @@
+//This file will export all the components of COMPONENT folder
+
+export { default as Cards } from './Cards/Cards';
+export { default as Chart } from './Chart/Chart';
+export { default as CountrySelector } from './CountrySelector/CountrySelector';
